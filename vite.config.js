aaa51750095201368b2c7f8948591308de7018b7
Raj <itsrajdesai17@gmail.com>
2025-02-09 02:48:06 +0530

    vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,16 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js'
   },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+    outDir: 'dist'
+  },
   define: {
     'process.env': {
-      VITE_SPEECHIFY_API_KEY: JSON.stringify(process.env.VITE_SPEECHIFY_API_KEY),
-      VITE_SPEECHIFY_API_URL: JSON.stringify(process.env.VITE_SPEECHIFY_API_URL)
+      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL),
+      VITE_SPEECHIFY_API_KEY: JSON.stringify(process.env.VITE_SPEECHIFY_API_KEY)
     }
   }
 })
